0d6fce4c7d4123706a160134aa7a73b73e49088b
MinKhant-MC <sysadmin@mochaauthentic.com>
2026-07-08 11:17:54 +0630

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   'use strict';
 
   window.GYM_CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbzj7lTJmvKBfj3kT4aOm7QDgy_HsKHgxo0BCBU8gKE93aiXfZ9YECFGBxTlq8hRjZ6D/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbwVh9MGqvpGqNYo7rdFrYv-lGw9wXT9o1Nq79N8QP75MimJ0LsL8vbwfwPTMiQhLGrF/exec',
     QR_IMAGE_URL: 'https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=',
     MEMBER_CACHE_TTL_MS: 7 * 24 * 60 * 60 * 1000,
     STORAGE_KEYS: {
